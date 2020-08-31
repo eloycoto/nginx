fd5093d164be5d0ae0be5c25325578f0c359a851
Eloy Coto <eloy.coto@acalustra.com>
2020-08-31 11:57:22 +0200

    Terminate master process when worker process received a kill -9
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- src/os/unix/ngx_process.c
+++ src/os/unix/ngx_process.c
@@ -399,6 +399,7 @@ ngx_signal_handler(int signo, siginfo_t *siginfo, void *ucontext)
             break;
 
         case SIGCHLD:
+            ngx_terminate = 1;
             ngx_reap = 1;
             break;
         }
